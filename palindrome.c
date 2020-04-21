f0063efa1516217c253c1ac7fbec2c19099d3237
sportokalidis <45517041+sportokalidis@users.noreply.github.com>
2020-04-22 02:28:12 +0300

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -34,14 +34,13 @@ palindrome_loop
 
 	MOV r2, #0
 	STR r2, [r1]
+		
 	BX lr
 
 palindrome_exit
-	// MOV r1, #1
 	MOV r2, #1
 	STR r2, [r1]
-	// LDR r1, [r2]
-	
+
 	BX lr
 }
 
@@ -67,5 +66,6 @@ int main() {
 	palindrome(str, result);
 	
 	while (1) {
+		
 	}
 }
