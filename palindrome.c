2a127fdf4dea23a1a769e1383564d2275f7d404c
sportokalidis <45517041+sportokalidis@users.noreply.github.com>
2020-05-02 01:44:32 +0300

    Add files via upload

--- palindrome.c
+++ palindrome.c
@@ -1,3 +1,21 @@
+/* 
+ * File: palindrome.c 
+ *
+ *
+ * Description: 1st Assignment for "Microprocessors and Peripherals" course 
+ *
+ *
+ * Authors: Christoforidis Savvas, schristo@ece.auth.gr
+ *          Portokalidis Stavros , stavport@ece.auth.gr
+ *    
+ *
+ * Date: 3 May 2020
+ *
+ * 
+*/
+
+
+
 #include <stdio.h>
 
 
@@ -11,11 +29,11 @@ init_loop
 	ADDS  r2, #1         // Increment size counter
 	CMP   r3, #0         // Was the byte 0?
 	BNE   init_loop	     // If not, repeat the loop
-	SUBS r2, r2, #2	     // r2 holds the size-1 of the string (without \0)
-	SUBS r0, r0, #2	     // r0 points the final position of str (before "\0")
+	SUBS r2, r2, #2			 // r2 holds the size-1 of the string (without \0)
+	SUBS r0, r0, #2			 // r0 points the final position of str (before "\0")
 
- 	MOV r4, r0           // r4 points the final position of str (before "\0")
-	SUBS r0, r0, r2      // r0 points the first character of str
+ 	MOV r4, r0			 		 // r4 points the final position of str (before "\0")
+	SUBS r0, r0, r2			 // r0 points the first character of str
 
 	
 palindrome_loop
@@ -26,14 +44,14 @@ palindrome_loop
 	CMP r0, r4           
 	BGE palindrome_exit  // if r0 >= r4 go to palindrome_exit label
                        
-		             // Else											 
+                       // Else											 
 	ADDS r0, r0, #1      // r0 points to next memory address
 	SUBS r4, r4, #1      // r4 points to previous memory address  
 
 	CMP r3, r2
 	BEQ palindrome_loop  // if r3 == r2 go to palindrome_loop label
 
-                             // Else												
+                       // Else												
 	MOV r2, #0           // r2 take the value 0
 	STR r2, [r1]         // Store byte in r2 into memory pointed to by r1 (result)
 	
