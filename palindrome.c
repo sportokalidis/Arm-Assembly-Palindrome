6ea299786b364531595f3ce300ebdca7d7cd8c55
sportokalidis <45517041+sportokalidis@users.noreply.github.com>
2020-05-01 22:00:13 +0300

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -11,11 +11,11 @@ init_loop
 	ADDS  r2, #1         // Increment size counter
 	CMP   r3, #0         // Was the byte 0?
 	BNE   init_loop	     // If not, repeat the loop
-	SUBS r2, r2, #2			 // r2 holds the size-1 of the string (without \0)
-	SUBS r0, r0, #2			 // r0 points the final position of str (before "\0")
+	SUBS r2, r2, #2	     // r2 holds the size-1 of the string (without \0)
+	SUBS r0, r0, #2	     // r0 points the final position of str (before "\0")
 
- 	MOV r4, r0			 		 // r4 points the final position of str (before "\0")
-	SUBS r0, r0, r2			 // r0 points the first character of str
+ 	MOV r4, r0           // r4 points the final position of str (before "\0")
+	SUBS r0, r0, r2      // r0 points the first character of str
 
 	
 palindrome_loop
@@ -26,14 +26,14 @@ palindrome_loop
 	CMP r0, r4           
 	BGE palindrome_exit  // if r0 >= r4 go to palindrome_exit label
                        
-                       // Else											 
+		             // Else											 
 	ADDS r0, r0, #1      // r0 points to next memory address
 	SUBS r4, r4, #1      // r4 points to previous memory address  
 
 	CMP r3, r2
 	BEQ palindrome_loop  // if r3 == r2 go to palindrome_loop label
 
-                       // Else												
+                             // Else												
 	MOV r2, #0           // r2 take the value 0
 	STR r2, [r1]         // Store byte in r2 into memory pointed to by r1 (result)
 	
